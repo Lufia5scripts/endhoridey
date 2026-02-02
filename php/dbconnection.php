5ecc5669e44e7da2963db87b0892c61f27e4cbca
Lufia5scripts <rackzyitz@gmail.com>
2026-02-02 19:57:54 +0000

    Create dbconnection.php

---
+++ php/dbconnection.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection snippet
+
+$servername = "localhost";
+$username = "root";
+$dbpassword = "";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
